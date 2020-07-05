2a1555e42ff88bdf6fa44e931e091cf6b22054bc
modernfaust <edward.l1245@hotmail.com>
2020-07-05 15:30:55 -0400

    update scripts for NPC move

---
+++ Assets/TacticsCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsCamera : MonoBehaviour 
+{
+    public void RotateLeft()
+    {
+        transform.Rotate(Vector3.up, 90, Space.Self);
+    }
+
+    public void RotateRight()
+    {
+        transform.Rotate(Vector3.up, -90, Space.Self);
+    }
+}
