a2867d6f3cfd240e393a26319cfc397976022a4a
modernfaust <edward.l1245@hotmail.com>
2020-07-06 11:26:34 -0400

    create template classes for dashpanel, units

---
+++ Assets/Actions/NPCMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMove : TacticsMove 
+{
+    GameObject target;
+
+	// Use this for initialization
+	void Start () 
+	{
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        Debug.DrawRay(transform.position, transform.forward);
+
+        if (!turn)
+        {
+            return;
+        }
+
+        if (!moving)
+        {
+            FindNearestTarget();
+            CalculatePath();
+            FindSelectableTiles();
+            actualTargetTile.target = true;
+        }
+        else
+        {
+            Move();
+        }
+	}
+
+    void CalculatePath()
+    {
+        Tile targetTile = GetTargetTile(target);
+        FindPath(targetTile);
+    }
+    
+    void FindNearestTarget()
+    {
+        //this ONLY looks for the nearest player unit. Replace this with a more sophisticated method
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Player");
+
+        GameObject nearest = null;
+        float distance = Mathf.Infinity;
+
+        foreach (GameObject obj in targets)
+        {
+            float d = Vector3.Distance(transform.position, obj.transform.position);
+            
+            if (d < distance)
+            {
+                distance = d;
+                nearest = obj;
+            }
+        }
+
+        target = nearest;
+    }
+}
---
+++ Assets/Units/Class.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class : MonoBehaviour
+{
+    int health;
+    int damage;
+    int defense;
+
+    int LevelUp(){
+        return 1;
+    }
+
+}
